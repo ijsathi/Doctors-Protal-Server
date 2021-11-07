b55610d8471b56dcd5b777053316be7d2ea355c2
ijsathi <ijsathi2001rs@gmail.com>
2021-11-07 21:35:36 +0600

    booking + user address add

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const { MongoClient } = require('mongodb');
 
 const port = process.env.PORT || 5000;
  
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vsocy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -14,10 +15,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
       await client.connect();
+      const database = client.db("doctors_portal");
+      const appointmentCollection = database.collection("appointments");
+
+      app.post('/appointments', async (req, res) =>{
+        const appointment = req.body;
+        const result = await appointmentCollection.insertOne(appointment);
+        console.log(result);
+        res.json(result);
+      })
      
     } 
     finally {
-      await client.close();
+      // await client.close();
     }
   }
 run().catch(console.dir);
